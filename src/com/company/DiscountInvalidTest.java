81f2179d490388df9c8385e80a1664af0e8a3e2a
WEBNIK19 <elit15223@ssu.edu.ua>
2019-12-27 13:28:17 +0200

    DiscountInvalidTest

--- src/com/company/DiscountInvalidTest.java
+++ src/com/company/DiscountInvalidTest.java
@@ -4,27 +4,23 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import java.util.Random;
-import java.util.Arrays;
 
+import java.util.Arrays;
+import java.util.Random;
 
 import static org.junit.Assert.*;
-
 @RunWith(value = Parameterized.class)
-
-public class DiscountValidTest {
-
+public class DiscountInvalidTest {
     private int quantity;
     private int type;
     private double expected;
     private ShoppingCart cart;
-
     @Before
     public void initialize(){
         this.cart = new ShoppingCart();
     }
 
-    public DiscountValidTest(int type) {
+    public DiscountInvalidTest(int type) {
         Random r = new Random();
         this.type = type;
         this.quantity = r.nextInt(99) + 1;
@@ -33,6 +29,8 @@ public class DiscountValidTest {
 
     public static double getExpected(int type, int quantity){
         double dis = 0;
+        Random r = new Random();
+        double rand = 0.0;
         switch (type) {
             case ShoppingCart.ITEM_REGULAR:
                 dis = (dis + (quantity / 100) * 0.1 - 0.8 < 0) ? (dis + (quantity / 100) * 0.1) :  0.8;
@@ -49,8 +47,11 @@ public class DiscountValidTest {
                 dis = 0.9;
         }
 
+        do{
+            rand = r.nextFloat();
+        }while(Math.abs(rand - dis) < 0.001);
 
-        return 1.0 - dis;
+        return 1.0 - rand;
     }
     @Parameterized.Parameters(name = "{index} : Type {0} : Quantity {1}")
     public static Iterable<Object[]> dataForTest(){
@@ -64,9 +65,9 @@ public class DiscountValidTest {
     }
 
     @Test
-    public void testDiscount() {
+    public void calculateDiscount() {
         cart.addItem("Item",5.00, quantity, type);
         double discount = ShoppingCart.calculateDiscount((ShoppingCart.Item)cart.getItem());
-        assertTrue("Expected "+ expected + " but was got " + discount + " quantity: " + quantity,Math.abs(expected - discount) < 0.001);
+        assertFalse("Expected "+ expected + " but was got " + discount + " quantity: " + quantity,Math.abs(expected - discount) < 0.001);
     }
 }
